6a599eb86a6ef17c64f42a2b398fe491a0f44341
MeenalGoyalTesting <meenal.goyal@testingxperts.com>
2026-05-29 17:32:36 +0530

    Docker and postgres

--- src/main/java/com/JobApplication/firstjobapp/FirstjobappApplication.java
+++ src/main/java/com/JobApplication/firstjobapp/FirstjobappApplication.java
@@ -2,11 +2,12 @@ package com.JobApplication.firstjobapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import java.util.TimeZone;
 @SpringBootApplication
 public class FirstjobappApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(FirstjobappApplication.class, args);
 	}
 
